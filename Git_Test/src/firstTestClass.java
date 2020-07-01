f533a0508753e0be633d178251c5388568f574d5
sindhuja <sindhujaramesh10@gmail.com>
2020-06-30 22:09:24 -0500

    commit from develop branch

--- Git_Test/src/firstTestClass.java
+++ Git_Test/src/firstTestClass.java
@@ -3,7 +3,7 @@ public class firstTestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+      System.out.println("This is the master branch");
 	}
 
 }
---
+++ Git_Test/src/secondTestClass.java
@@ -0,0 +1,10 @@
+
+public class secondTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the second class from master");
+
+	}
+
+}
